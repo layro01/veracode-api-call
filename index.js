e5f3b88a37dda2cf59c07619015d51a7490f1799
Robert Layzell <rlayzell@veracode.com>
2023-08-08 19:06:03 -0400

    Tidy up

--- index.js
+++ index.js
@@ -11,31 +11,18 @@ try {
   const endpointUrl = core.getInput('endpoint-url');
   const apiMethod = core.getInput('api-method');
   const apiBody = core.getInput('api-body');
-
-  console.log(`Got input parameters:`);
-  console.log(`  base-url: ${baseUrl}`);
-  console.log(`  endpoint-url: ${endpointUrl}`);
-  console.log(`  api-method: ${apiMethod}`);
-  console.log(`  api-body: ${apiBody}`);
-
   const fullUrl = new URL(endpointUrl, baseUrl);
-  console.log(`Calling ${apiMethod} ${fullUrl.href}...`);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 
   // Get the Veracode API Key and Secret from action secrets stored in the repo executing a workflow using this action.
   const apiKeyId = process.env.VERACODE_API_KEY_ID;
   const apiKeySecret = process.env.VERACODE_API_KEY_SECRET;
-  if (apiKeyId !== undefined && apiKeyId.length > 0) {
-    console.log('Got API Key ID');
-  }
-  if (apiKeySecret !== undefined && apiKeySecret.length > 0) {
-    console.log('Got API Key Secret');
-  }  
 
   // Generate an HMAC header for the call.
   const authorization = auth.generateAuthHeader(apiKeyId, apiKeySecret, fullUrl.href, apiMethod);
-  if (authorization !== undefined && authorization.length > 0) {
-    console.log('Generated HMAC header for request');
-  } 
 
   // Make the REST API call.
   const headers = {
@@ -47,32 +34,27 @@ try {
     case 'GET':
       console.log('Making GET call');
       axios.get(fullUrl, {headers: headers}).then(response => {
-        console.log(`Response: ${JSON.stringify(response.data)}`);
         core.setOutput("response", JSON.stringify(response.data));
       })
       break;
     case 'HEAD':
       axios.head(fullUrl, {headers: headers}).then(response => {
-        core.setOutput("response", response);
+        core.setOutput("response", JSON.stringify(response.data));
       })
       break;
     case 'PATCH':
       axios.patch(fullUrl, apiBody, {headers: headers}).then(response => {
-        core.setOutput("response", response);
+        core.setOutput("response", JSON.stringify(response.data));
       })
       break;
     case 'POST':
       axios.post(fullUrl, apiBody, {headers: headers}).then(response => {
-        core.setOutput("response", response);
+        core.setOutput("response", JSON.stringify(response.data));
       })
       break;
     default:
       throw new Error(`Unsupported HTTP method '${apiMethod}'`);
   }
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
 
 } catch (error) {
   core.setFailed(error.message);
